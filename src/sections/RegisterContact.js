3fab542e0f79cf425c0671d215c8d627ecc1f214
JumnerArch <1frankjus@hdsb.ca>
2021-11-22 21:45:13 -0500

    Done register

--- src/sections/RegisterContact.js
+++ src/sections/RegisterContact.js
@@ -7,6 +7,19 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
+const Icon = ({ text, url, network }) => {
+  return (
+    <HStack pl={{ sm: 5, md: 20 }} spacing={12} pb="6">
+      <SocialIcon target="_blank" network={network} url={url} />
+      <Thin fontSize="1rem">
+        <Link href={url} isExternal>
+          {text}
+        </Link>
+      </Thin>
+    </HStack>
+  );
+};
+
 const RegisterContact = (args) => {
   useEffect(() => {
     gsap.from(".rcHeading", {
@@ -39,54 +52,23 @@ const RegisterContact = (args) => {
       </div>
       <div className="rcIcons">
         <VStack direction="row" w="full" pt="16" alignItems="flex-start">
-          <HStack pl="20" spacing={12} pb="6">
-            <SocialIcon
-              target="_blank"
-              network="sharethis"
-              url="https://docs.google.com/forms/d/1r8_S_RepQEqpBaR2SHO2B7iBX1AmAPgGZAg4JyorVmM/edit?usp=sharing"
-            />
-            <Thin fontSize="1rem">
-              <Link
-                href="https://docs.google.com/forms/d/1r8_S_RepQEqpBaR2SHO2B7iBX1AmAPgGZAg4JyorVmM/edit?usp=sharing"
-                isExternal
-              >
-                register here!
-              </Link>
-            </Thin>
-          </HStack>
-
-          <HStack pl="20" spacing={12} pb="6">
-            <SocialIcon network="mailto" url="mailto:hacktheridge@gmail.com" />
-            <Thin fontSize="1rem">
-              <Link href="mailto:hacktheridge@gmail.com" isExternal>
-                hacktheridge@gmail.com
-              </Link>
-            </Thin>
-          </HStack>
-
-          <HStack pl="20" spacing={12} pb="6">
-            <SocialIcon
-              target="_blank"
-              url="https://www.instagram.com/hacktheridge/"
-              network="instagram"
-              isExternal
-            />
-            <Thin fontSize="1rem">
-              <Link href="https://www.instagram.com/hacktheridge/" isExternal>
-                @hacktheridge
-              </Link>
-            </Thin>
-          </HStack>
-
-          <HStack pl="20" spacing={12} pb="6">
-            <SocialIcon network="discord" />
-            <Thin fontSize="1rem">coming soon!</Thin>
-          </HStack>
-
-          <HStack pl="20" spacing={12} pb="6">
-            <SocialIcon network="rss" />
-            <Thin fontSize="1rem">devpost: coming soon!</Thin>
-          </HStack>
+          <Icon
+            text="register here!"
+            url="https://docs.google.com/forms/d/1r8_S_RepQEqpBaR2SHO2B7iBX1AmAPgGZAg4JyorVmM/edit?usp=sharing"
+            network="sharethis"
+          />
+          <Icon
+            text="hacktheridge@gmail.com"
+            url="mailto:hacktheridge@gmail.com"
+            network="mailto"
+          />
+          <Icon
+            text="@hacktheridge"
+            url="https://www.instagram.com/hacktheridge/"
+            network="instagram"
+          />
+          <Icon text="coming soon!" url={null} network="discord" />
+          <Icon text="devpost: coming soon!" url={null} network="rss" />
         </VStack>
       </div>
     </VStack>
